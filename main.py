82c736e7ade810198af92453d8db15fb82fc5f71
G90SG <71718345+G90SG@users.noreply.github.com>
2021-02-21 13:46:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-#Explaining the aim of the program to the user#
+#Explaining the aim of the program to the user
 print("Please give me a word and I'll tell you if it's a Palindrome. ")
-
-#Asking the user to input a word#
+#Asking the user to input a  assigning it to the Variable Word and categorising as a string
 word = str(input("What is your chosen word? "))
+# Reversal of the input
 reverse = "".join(reversed(word)) 
-#reversal of the input
-#check whether the input is equal to it's reverse or not
+# Check whether the input is equal to it's reverse or not and this determines whether it is a Palindrome
 if(word == reverse): 
     print("Yes, the word " +word+ " is palindrome.")
 else:
